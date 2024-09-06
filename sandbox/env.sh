4086a7f9391a7dcc4e25a81ba897ab043a11a412
garywei944 <33930674+garywei944@users.noreply.github.com>
2024-09-06 03:04:31 -0700

    working on collect sysinfo

--- sandbox/env.sh
+++ sandbox/env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Install python
 export MAMBA_ROOT_PREFIX="$HOME/micromamba"
 
 eval "$(micromamba shell hook --shell bash)"
 
-micromamba -y create -n eden python=3.12 pip -c conda-forge
+micromamba -y create -n eden -c conda-forge \
+    "python>=3.10" \
+    psutil \
+    distro \
+    attrs \
+    absl-py
 micromamba activate eden
