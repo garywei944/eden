3d8e1fed7fdbbda743eef3bd6f8a419aa6e23d80
garywei944 <33930674+garywei944@users.noreply.github.com>
2026-01-03 19:19:20 -0500

    Update __main__

--- eden/__main__.py
+++ eden/__main__.py
@@ -3,9 +3,9 @@ from eden.utils.logging_utils import setup_root_logger
 setup_root_logger()
 
 import logging
-from pathlib import Path
 
-from sandbox.context import RuntimeContext
+# from pathlib import Path
+
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +13,7 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info("Eden main module executed")
 
-    project_root = Path(__file__).parent.parent.resolve()
-    RuntimeContext.build(project_root=project_root)
+    # project_root = Path(__file__).parent.parent.resolve()
 
 
 main()
