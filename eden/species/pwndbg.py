b4468ab26e2d4434e8cf722d9b053c2f61889762
garywei944 <33930674+garywei944@users.noreply.github.com>
2026-01-12 11:14:55 -0800

    feat: add pwndbg

---
+++ eden/species/pwndbg.py
@@ -0,0 +1,41 @@
+import shutil
+from pathlib import Path
+
+from eden.context import Context
+from eden.eva import Eva
+from eden.sh import esh as sh
+
+VERSION = "2025.10.20"
+
+ctx = Context.instance()
+eva = Eva.instance()
+
+# ! pandbg is not supported without sudo
+if not eva.sudo:
+    raise RuntimeError("pwndbg installation requires sudo privileges")
+
+if ctx.os_id == "arch":
+
+    def post_install():
+
+        with Path.home().joinpath(".gdbinit").open("w", encoding="utf-8") as gdbinit:
+            gdbinit.write("source /usr/share/pwndbg/gdbinit.py\n")
+
+else:
+    depends = ["git", "sudo", "python"]
+
+    def install():
+        with sh.pushd(Path.home() / ".local" / "share"):
+            shutil.rmtree("pwndbg", ignore_errors=True)
+            sh.git.clone("https://github.com/pwndbg/pwndbg")
+            with sh.pushd("pwndbg"):
+                if ctx.os_id == "debian" and ctx.os_version.major == 10:
+                    sh.git.checkout("debian10-final")
+                else:
+                    sh.git.checkout(VERSION)
+
+                sh.Command("./setup.sh")()
+
+    def post_install():
+        with Path.home().joinpath(".gdbinit").open("w", encoding="utf-8") as gdbinit:
+            gdbinit.write("source ~/.local/share/pwndbg/gdbinit.py\n")
